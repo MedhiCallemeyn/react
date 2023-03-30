5707b8ca73198adbecd7fc78a1f7746b5ca8c9eb
MedhiCallemeyn <callemeynmedhihub@gmail.com>
2023-03-30 04:43:19 +0200

    petit projet decouverte react JS

---
+++ country-app/src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe sed
+        cumque ipsa incidunt minima consequuntur, amet aliquam! Cumque facere
+        quisquam iure deserunt quidem, suscipit doloribus illum et. Sequi, atque
+        ab!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo blanditiis
+        sequi illo, perferendis animi maxime ipsum? Quibusdam accusantium sunt
+        vitae temporibus quas cumque minima, iure et ad deleniti alias iste.
+      </p>
+    </div>
+  );
+};
+
+export default About;
